86967647692dad217b22dd4a3a12e8732ee2ca42
Underminer <244198756@qq.com>
2023-02-27 17:43:11 +0800

    Index Triangle with Color
    render with color

--- Underminer-Core/Underminer-Sandbox/Window.cs
+++ Underminer-Core/Underminer-Sandbox/Window.cs
@@ -17,10 +17,10 @@ namespace Underminer_Sandbox
 
         float[] _vertices =
         {
-             0.5f,  0.5f, 0.0f,     // 右上角
-             0.5f, -0.5f, 0.0f,     // 右下角
-            -0.5f, -0.5f, 0.0f,     // 左下角
-            -0.5f,  0.5f, 0.0f      // 左上角
+             0.5f,  0.5f, 0.0f, 1, 0, 0,    // 右上角
+             0.5f, -0.5f, 0.0f, 0, 1, 0,    // 右下角
+            -0.5f, -0.5f, 0.0f, 0, 0, 1,    // 左下角
+            -0.5f,  0.5f, 0.0f, 1, 0, 1     // 左上角
         };
 
         uint[] _indices = {
@@ -44,7 +44,7 @@ namespace Underminer_Sandbox
         protected override void OnLoad()
         {
             // 绘制线框
-            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            // GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
             
             // 绑定vao 为了指定layout
             _vao = GL.GenVertexArray();
@@ -56,19 +56,28 @@ namespace Underminer_Sandbox
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
             // 缓冲区类型 缓冲区大小 要传的数据 传入类型 (static不动 Dynamic少动 Stream多动)
             GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            // location位置0 指向aPos aPos内容格式
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            // location位置1 指向aColor
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            #region shader创建与绑定pipline
+
             // 顶点shader代码 (@转义)
-            string vertexSource = @"#version 330 core
+            string vertexSource = @"
+                #version 460 core
                 layout (location = 0) in vec3 aPos;
                 layout (location = 1) in vec3 aColor;
+
+                layout (location = 0) out vec3 color;
 
                 void main()
                 {
                     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+                    color = aColor;
                 }";
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
-
-            #region shader创建与绑定pipline
 
             int vertexShader = GL.CreateShader(ShaderType.VertexShader);
             // 将shader内容与类型绑定
@@ -77,12 +86,14 @@ namespace Underminer_Sandbox
             GL.CompileShader(vertexShader);
 
             // 片段shader代码 
-            string fragmentSource = @"#version 330 core
+            string fragmentSource = @"
+                #version 460 core
                 out vec4 FragColor;
+                layout (location = 0) in vec3 color;
 
                 void main()
                 {
-                    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+                    FragColor = vec4(color, 1.0f);
                 } ";
 
             int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
@@ -105,9 +116,9 @@ namespace Underminer_Sandbox
 
             // 创建索引缓冲对象
             _ebo = GL.GenBuffer();
+            // 顶点位置数据
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
             GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
-
 
         }
 
